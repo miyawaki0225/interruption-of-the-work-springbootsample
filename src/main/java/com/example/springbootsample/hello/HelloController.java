2aadb237e59d8ff50718610d9a775f844f4057f7
miyawaki0225 <m-miywaki@rapide-act.com>
2022-07-06 23:40:36 +0900

    Add response.html

--- src/main/java/com/example/springbootsample/hello/HelloController.java
+++ src/main/java/com/example/springbootsample/hello/HelloController.java
@@ -1,12 +1,23 @@
 package com.example.springbootsample.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+    //GET
     @GetMapping("/hello")
     public String getHello(){
         return "hello";
+    }
+
+    //POST
+    @PostMapping("/hello")
+    public String postRequest(@RequestParam("text1") String str,Model model){
+        model.addAttribute("sample",str);
+        return "hello/response";
     }
 }
