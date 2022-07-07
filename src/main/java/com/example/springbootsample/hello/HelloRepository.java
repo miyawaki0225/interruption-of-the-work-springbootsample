6aeb5d3cd557d828c11a69514f09f061e2315532
miyawaki0225 <108254626+miyawaki0225@users.noreply.github.com>
2022-07-07 21:10:59 +0900

    Add db.html

--- src/main/java/com/example/springbootsample/hello/HelloRepository.java
+++ src/main/java/com/example/springbootsample/hello/HelloRepository.java
@@ -2,11 +2,13 @@ package com.example.springbootsample.hello;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class HelloRepository {
+    @Autowired
     private JdbcTemplate jdbcTemplate;
     
     public Map<String, Object>findById(String id){
